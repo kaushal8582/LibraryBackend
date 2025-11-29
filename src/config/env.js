9e442dc63382df27e84c4f246cb91a0f3b709061
developerSTechGit <developers.access.st@gmail.com>
2025-11-29 20:07:36 +0530

    fix env

--- src/config/env.js
+++ src/config/env.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
-  PORT: process.env.PORT || 5001, // Changed to 5001 to avoid port conflict
+  PORT: process.env.PORT || 5001, 
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/libtrack',
-  CLIENT_URL:process.env.CLIENT_UR || "http://localhost:3000",
+  CLIENT_URL:process.env.CLIENT_URL || "http://localhost:3000",
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '1d',
